fe1468107c88f861a66e378ec4728178c6e2107c
skashyap9934 <skashyap9934@gmail.com>
2024-06-15 19:21:47 +0530

    walmart client

--- server/index.js
+++ server/index.js
@@ -12,9 +12,7 @@ const PORT = process.env.PORT || 8080;
 const app = express();
 
 app.use(express.json());
-app.use(cors({
-  origin: "https://walmart-clone-frontend.vercel.app"
-}));
+app.use(cors({}));
 app.use("/user", userRouter);
 app.use("/products", productsRouter);
 app.use("/wishlist", wishlistsRouter);
